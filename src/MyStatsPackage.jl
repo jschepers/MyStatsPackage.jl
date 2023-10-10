a2211d3561a7b3c9fd3dcb29b0c427494cf789b4
jschepers <judith.schepers@vis.uni-stuttgart.de>
2023-10-10 08:39:29 +0000

    added printOwner function to module

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -1,7 +1,10 @@
 module MyStatsPackage
 include("statistic_functions.jl")
+include("printOwner.jl")
 export rse_sum
 export rse_mean
 export rse_std
 export rse_tstat
+
+export printOwner
 end # module MyStatsPackage
