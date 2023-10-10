01d21fd9cdfa80f687722038f72ea304cb618314
jschepers <judith.schepers@vis.uni-stuttgart.de>
2023-10-10 08:25:10 +0000

    Added statistical functions

---
+++ src/MyStatsPackage.jl
@@ -0,0 +1,7 @@
+module MyStatsPackage
+include("statistic_functions.jl")
+export rse_sum
+export rse_mean
+export rse_std
+export rse_tstat
+end # module MyStatsPackage
---
+++ src/statistic_functions.jl
@@ -0,0 +1,41 @@
+function rse_sum(myrange)
+    res = 0
+    for i in myrange
+        res += i
+    end
+    return res
+end
+
+function rse_mean(myrange)
+    return rse_sum(myrange)/length(myrange)
+end
+
+function rse_std(myrange)
+    myvector = collect(myrange)
+    return sqrt(sum((myvector .- rse_mean(myrange)).^2)/(length(myrange)-1))
+end
+
+function rse_tstat(myrange;σ=rse_std(myrange))
+    return rse_mean(myrange)/(σ/sqrt(length(myrange)))
+end
+
+struct StatResult
+    x::UnitRange{Int64}
+    n::Int64
+    std::Float64
+    tvalue::Float64
+end
+
+function StatResult(myrange)
+    x = myrange
+    n = length(x)
+    std = rse_std(x)
+    tvalue = rse_tstat(x)
+    StatResult(x,n,std,tvalue)
+end
+
+import Base: length
+
+function Base.length(s::StatResult)
+    return s.n
+end
