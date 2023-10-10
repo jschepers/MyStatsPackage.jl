5d9270993e3ca8a262871f251bf0f11defbdeee6
David Schneider <david.schneider@ipvs.uni-stuttgart.de>
2023-10-10 10:54:39 +0200

    Add print contributor

--- src/MyStatsPackage.jl
+++ src/MyStatsPackage.jl
@@ -1,10 +1,12 @@
 module MyStatsPackage
 include("statistic_functions.jl")
 include("printOwner.jl")
+include("printContributor.jl")
 export rse_sum
 export rse_mean
 export rse_std
 export rse_tstat
 
 export printOwner
+export printContributor
 end # module MyStatsPackage
---
+++ src/printContributor.jl
@@ -0,0 +1,3 @@
+function printContributor()
+    print("davidscn")
+end
